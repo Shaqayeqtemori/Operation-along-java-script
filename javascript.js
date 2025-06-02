b481e9337a02a5b7d50ecea3a8617c7e02ffeab3
Shaqayeq Temori <edristemory33@gmail.com>
2025-06-02 09:03:46 +0430

    opeartion

---
+++ javascript.js
@@ -0,0 +1,12 @@
+// age checker 
+let nameofuser = "user of facebook";
+let agelenght = nameofuser .lenght ;
+if (agelenght > 18) {
+    console.log("You can register!");
+}
+else if(agelenght === 18){
+    console.log("you can also register and use it now.");
+}
+else{
+    console.log("sorry you can't register because you are under age!");
+}
